8969e4400c71ea4e69171e4eed88912f52cf430d
Ronaldo Onorio <ronaldoonorio@yahoo.com.br>
2022-12-29 14:37:48 -0300

    add

---
+++ FormZap3/js/form.js
@@ -0,0 +1,41 @@
+document.querySelector("#submit").addEventListener("click", e => {
+  e.preventDefault();
+
+  //INGRESE UN NUMERO DE WHATSAPP VALIDO AQUI:
+  const telefono = "67998763544";
+
+  const cliente = document.querySelector("#cliente").value;
+  const fecha = document.querySelector("#fecha").value;
+  const hora = document.querySelector("#hora").value;
+  const empleado = document.querySelector("#empleado").value;
+  const servicio = document.querySelector("#servicio").value;
+  const resp = document.querySelector("#respuesta");
+
+  resp.classList.remove("fail");
+  resp.classList.remove("send");
+
+  const url = `https://api.whatsapp.com/send?phone=${telefono}&text=
+		*Serviço*%0A
+		*Orçamento*%0A%0A
+		*Nome*%0A
+		${cliente}%0A
+		*Data*%0A
+		${fecha}%0A
+		*Horário*%0A
+		${hora}%0A
+		*Instalador*%0A
+		${empleado}%0A
+		*Serviço*%0A
+		${servicio}`;
+
+  if (cliente === "" || fecha === "" || hora === "") {
+    resp.classList.add("fail");
+    resp.innerHTML = `Faltan algunos datos, ${cliente}`;
+    return false;
+  }
+  resp.classList.remove("fail");
+  resp.classList.add("send");
+  resp.innerHTML = `Se ha enviado tu reserva, ${cliente}`;
+
+  window.open(url);
+});
